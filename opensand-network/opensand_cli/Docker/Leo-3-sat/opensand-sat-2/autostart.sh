6723a8187f3949f5341e5f4281be7046b008b49e
lucaFiscariello <lucafiscariello@gmail.com>
2023-11-22 22:33:04 +0100

    LEO

---
+++ opensand-network/opensand_cli/Docker/Leo-3-sat/opensand-sat-2/autostart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (c) 2023 RomARS
+# The code was developed by Mattia Quadrini <quadrini@romars.tech>
+
+
+# Start SSH service
+service ssh start 
+
+#Run SSHD daemon
+/usr/sbin/sshd -D
+
+make
+modprobe br_netfilter
+
+iptables -t nat -A PREROUTING -i eth0 -j DNAT --to-destination 192.168.0.6
+iptables -A FORWARD -i eth0 -d 192.168.0.6 -j ACCEPT
+
+# Keep alive
+sleep inf
